a469948fbdbadab0763f9678b4e65a60a8fcbaec
Skuli Dropek <66840575+skulidropek@users.noreply.github.com>
2021-06-17 19:38:40 +0300

    Ласт версия
    Ласт версия игры

--- Game/tds-game/Assets/Scripts/Player/PlayerShooting.cs
+++ Game/tds-game/Assets/Scripts/Player/PlayerShooting.cs
@@ -7,16 +7,29 @@ public class PlayerShooting : MonoBehaviour
     [SerializeField] private Transform _firePoint;
     [SerializeField] private GameObject _bulletObj;
     [SerializeField] private float _bulletForce;
+    [SerializeField] private float _cooldown;
+
+    private bool _fire;
+    private float _reloadTime;
     //[SerializeField] private LayerMask _maskIsSolid;
     // private int _distance = 2;
 
 
-
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        _fire = Input.GetButton("Fire1");
+    }
+
+    private void FixedUpdate()
+    {
+        if (_fire && _reloadTime <= 0)
         {
             Shoot();
+            _reloadTime = _cooldown;
+        }
+        else
+        {
+            _reloadTime -= Time.deltaTime;
         }
     }
 
